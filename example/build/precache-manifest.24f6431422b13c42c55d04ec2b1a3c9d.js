21c9afe4eafc22da2700b2f7de60e6fa256e8926
Lee Priest <lpleepriest@gmail.com>
2020-04-15 17:54:29 +0100

    Example build

---
+++ example/build/precache-manifest.24f6431422b13c42c55d04ec2b1a3c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c850335b65f1bfd4271374954bcd8bc",
+    "url": "/simple-react-carousel/index.html"
+  },
+  {
+    "revision": "a2d753745b422d2ac5f9",
+    "url": "/simple-react-carousel/static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "71ea6f019d02c1c1a54b",
+    "url": "/simple-react-carousel/static/js/2.44dbbe47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-carousel/static/js/2.44dbbe47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d753745b422d2ac5f9",
+    "url": "/simple-react-carousel/static/js/main.213caab7.chunk.js"
+  },
+  {
+    "revision": "ed39db8f93e8b8ecd557",
+    "url": "/simple-react-carousel/static/js/runtime-main.a9f7e1d7.js"
+  }
+]);
